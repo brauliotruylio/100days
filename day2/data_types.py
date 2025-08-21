7b306cc0cd3399a01007beb72fdee88b94ccebd0
Braulio <braulio.truylio@gmail.com>
2025-08-20 22:47:43 -0300

    dia2

---
+++ day2/data_types.py
@@ -0,0 +1,38 @@
+# print(len("123456"))
+# print(type(123))
+# print(type('123'))
+# print(type(12.34))
+# print(type(True))
+# print(type(None))
+# print(type([1, 2, 3]))
+# print(type((1, 2, 3)))
+# print(type({'key': 'value'}))
+# print(type({1, 2, 3}))
+# print(type(range(5)))
+# print(type(b'bytes'))
+# print(type(bytearray(5)))
+# print(type(memoryview(b'bytes')))
+# print(type(complex(1, 2)))
+# print(type(f"f-string {123}"))
+# print(type(print))  # Function type
+
+# print(int("123"))  # Convert string to integer
+# print(float("12.34"))  # Convert string to float
+# print(str(123))  # Convert integer to string
+# print(bool(1))  # Convert integer to boolean
+
+# # print(int("abc"))  # Convert string to integer
+# print(float("1234"))  # Convert string to float
+# print(str(123))  # Convert integer to string
+# print(bool(-1))
+
+# print("Number of letters in your name: " + str(len(input('What is your name? '))))
+# print(type(6 / 3))
+# print(type(6 // 3))
+# print(type(6 % 3))
+# print(type(6 ** 3))
+# print(type(6 + 3))
+# print(type(6 - 3))
+# print(type(6 * 3))
+# print((6 // 4))
+print(3 * 3 + 3 / 3 - 3)
---
+++ day2/imc.py
@@ -0,0 +1,10 @@
+height = 1.65
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+# bmi = weight / (height * height)
+# Or you can use the exponent operator **
+bmi = weight / height ** 2
+
+print(round(bmi, 2))
