68ecd27da38eee1f83afa0dd170c8f25a5673496
Braulio <braulio.truylio@gmail.com>
2025-08-20 22:50:15 -0300

    update

--- day2/tip_calculator.py
+++ day2/tip_calculator.py
@@ -1,11 +1,14 @@
+
 print("Bem vindo ao Calculador de Gorjetas!")
 print("Vamos calcular a gorjeta com base no valor da conta, a porcentagem de gorjeta e o número de pessoas.")
+
 total = float(input("Qual é o valor total da conta? R$ "))
 gorjeta_percentagem = float(input("Qual é a porcentagem de gorjeta? (ex: 10, 12, 15) "))
 pessoas = int(input("Quantas pessoas vão dividir a conta? "))
 gorjeta = (total * gorjeta_percentagem) / 100
 total_com_gorjeta = total + gorjeta
 valor_por_pessoa = total_com_gorjeta / pessoas
+
 print(f"\nTotal da conta: R$ {total:.2f}")
 print(f"Gorjeta: R$ {gorjeta:.2f} ({gorjeta_percentagem}%)")
 print(f"Total com gorjeta: R$ {total_com_gorjeta:.2f}")
