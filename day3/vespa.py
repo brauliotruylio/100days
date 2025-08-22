c2d1015284ed2f04289540ef341e7288b3ee3ec8
Braulio <braulio.truylio@gmail.com>
2025-08-21 21:42:23 -0300

    update

--- day3/vespa.py
+++ day3/vespa.py
@@ -15,15 +15,15 @@ print('''
        `-.__,-'       Vespa P-200 E       `-.__,-
 ''')
 
-print("Bem vindo ao jogo do tesouro!")
-print("Sua missão é encontrar o tesouro.")
+print("Bem vindo ao jogo da Vespa!")
+print("Sua missão é encontrar a Vespa.")
 direcao = input("Você está na encruzilhada. Para onde você quer ir? Digite 'esquerda' ou 'direita': ").lower()
 if direcao == "esquerda":
     acao = input("Você chegou a um lago. Digite 'esperar' para esperar por um barco ou 'nadar' para nadar até a ilha: ").lower()
     if acao == "esperar":
         porta = input("Você chegou à ilha. Há uma casa com 3 portas: uma vermelha, uma amarela e uma azul. Qual você escolhe? ").lower()
         if porta == "amarela":
-            print("Você encontrou o tesouro! Parabéns!")
+            print("Você encontrou a Vespa! Parabéns!")
         elif porta == "vermelha":
             print("Você entrou em uma sala cheia de fogo. Game Over.")
         elif porta == "azul":
